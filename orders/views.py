44e5e23f8073536b11ecf0083eabd31b3da4a0f1
Rapzzider <thecrisgb33@gmail.com>
2024-11-07 10:34:25 -0300

    Agregando cambio en producto

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Order
+from products.models import Product
+from .forms import OrderForm
 
 # Create your views here.
