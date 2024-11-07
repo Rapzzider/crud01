8142180e1d53e38e90ac2d7cbac75d863948b089
Rapzzider <thecrisgb33@gmail.com>
2024-11-07 12:06:17 -0300

    Agregando vista y template de Order

--- orders/views.py
+++ orders/views.py
@@ -4,3 +4,16 @@ from products.models import Product
 from .forms import OrderForm
 
 # Create your views here.
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'orders/order_list.html', {'orders': orders})
+
+def order_create(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('order_list')
+    else:
+        form = OrderForm()
+    return render(request, 'orders/order_form.html', {'form': form})
