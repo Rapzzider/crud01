481fb5bef6684b2b53368f18cf43e7d323d9c7a9
Rapzzider <thecrisgb33@gmail.com>
2024-11-07 10:14:14 -0300

    Agregando cambio en producto

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('created/', views.product_created, name='product_created'),
-    path('<int:id>/', views.product_detail, name='product_detail'),
-    path('<int:id>/updated/', views.product_updated, name='product_updated'),
-    path('<int:id>/deleted/', views.product_deleted, name='product_deleted'),
+    path('', views.product_list,name='product_list'),
+    path('created/', views.product_created, name='product_create'),
+    path('<int:id>/', views.product_detail,name='product_detail'),
+    path('<int:id>/update/',views.product_update, name='product_update'),
+    path('<int:id>/delete/',views.product_delete, name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -3,43 +3,38 @@ from .models import Product
 from .forms import ProductForm
 
 # Create your views here.
-def producto_create(request):
+def product_created(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('product_list')
     else:
-        form = ProductForm()
-    return render(request, 'products/product_form.html', {'form': form})
-#Listar productos
-def producto_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html',{'products': products})
-#leer detalle
-def producto_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    return render(request, 'products/product_detail.html', {'product': product})
+        form = ProductForm() 
+    return render(request, 'products/product_form.html',{'form':form})       
+#Listar productos      
+def product_list(request):
+    products  = Product.objects.all()
+    return render(request,'products/product_list.html',{'products': products})
+#Leer detalle
+def product_detail(request,id):
+    product = get_object_or_404(Product,pk=id)
+    return render(request, 'products/product_detail.html',{'product' : product})
 
-#Actualizar
-
-def producto_update(request, id):
-    product = get_object_or_404(Product, pk=id)
+def product_update(request,id):
+    product = get_object_or_404(Product,pk=id)
     if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
+        form= ProductForm(request.POST,instance=product)
         if form.is_valid():
             form.save()
             return redirect('product_list')
-        else:
-            form = ProductForm(instance=product)
-        return render(request, 'products/product_form.html', {'form': form})
+    else:
+        form = ProductForm(instance=product)
+    return render(request,'products/product_form.html',{'form': form})    
 
-#eliminar
-
-def producto_delete(request, id):
-    product = get_object_or_404(Product, pk=id)
+def product_delete(request,id):
+    product = get_object_or_404(Product,pk=id)
     if request.method == 'POST':
         product.delete()
         return redirect('product_list')
-    return render(request, 'products/product_confirm_delete.html', {'product': product})
-
+    return render(request, 'products/product_confirm_delete.html',{'product' : product})
